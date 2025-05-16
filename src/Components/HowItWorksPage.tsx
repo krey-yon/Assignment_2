79f374867f24845a917dc2f1739c00d54089d10c
Vikas <101702902+NotYourEren@users.noreply.github.com>
2025-05-16 13:29:08 +0530

    improved responsiveness

--- src/Components/HowItWorksPage.tsx
+++ src/Components/HowItWorksPage.tsx
@@ -3,13 +3,14 @@ import StepsCard from "./StepsCard";
 
 const HowItWorksPage = () => {
   return (
-    <div className="flex flex-col lg:grid lg:grid-cols-2 bg-white relative items-start lg:items-center px-4 sm:px-6 md:px-8 lg:px-0">
+    <div className="flex flex-col lg:grid lg:grid-cols-2 bg-white relative items-start lg:items-center px-4 sm:px-6 md:px-8 lg:px-0 max-w-[1480px] mx-auto">
       <img
         src="/images/virticallines.webp"
         alt="verticalLines"
         className="absolute top-0 left-0 lg:left-[150px] hidden lg:block"
         width={100}
       />
+
       <div className="flex flex-col w-full lg:w-[440px] lg:ml-[150px] py-6 lg:py-0">
         <h1 className="text-blue-900 font-black text-2xl sm:text-3xl lg:text-4xl pb-2">
           India's Biggest Media Database
@@ -20,7 +21,8 @@ const HowItWorksPage = () => {
           since the 1500s...
         </p>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 sm:gap-6 w-full pb-8 sm:pb-12 lg:pb-[100px] pt-6 sm:pt-8">
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 sm:gap-6 w-full pb-8 sm:pb-12 lg:pb-[100px] pt-6 sm:pt-8 justify-items-center sm:justify-items-start lg:justify-items-center">
         {stepsCardData.map((card) => (
           <StepsCard
             key={card.id}
